37dc2d2e1af401117a54b209e1ec3ad65a21a9ac
soso <s.k.1117.a.b.c@gmail.com>
2023-03-15 10:43:40 +0900

    multiLogin

---
+++ .history/app/Http/Middleware/Authenticate_20230310111338.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
+
+class Authenticate extends Middleware
+{
+
+    protected $user_route = 'user.login';
+    protected $owner_route = 'owner.login';
+    protected $admin_route = 'admin.login';
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     */
+    protected function redirectTo(Request $request): ?string
+    {
+       
+    }
+}
